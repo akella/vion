55d99012056ae291e903e70d3cabc74f49904d5b
mirantel <magi.mirantel@gmail.com>
2012-11-30 19:53:21 +0200

    fix

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	$('#scrollbar1').tinyscrollbar();
+
+	$(function() {
+
+		$('.select__sel').styler({
+			browseText: 'Обзор...',
+			zIndex: '9999'
+		});
+	});
+	
 });
