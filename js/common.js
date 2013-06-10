082ce70c5921d7b995e90f79fce488e9e6d569c0
hraboviyvadim <hraboviyvadim@gmail.com>
2013-06-10 12:32:46 +0300

    chat complete + pop-ups

--- js/common.js
+++ js/common.js
@@ -16,4 +16,26 @@ $(document).ready(function() {
 			$('.video-player__more').fadeOut();
 			}
 	);
+
+	$(".js-tab-cont").hide();
+	//$(".chat__icons a:first").addClass("js-chat-tabs");
+	$(".js-tab1").show();
+	$(".chat__icons a:first").addClass("is-active");
+	// $('.chat__icons a').click(function(e) {
+	// 	$(".chat__tab-items div").hide();
+	// 	$(".chat__icons a").removeClass("js-chat-tabs");
+	// 	$(this).addClass("js-chat-tabs");
+	// });
+    $(".chat__icons a").click(function(){
+    	var index = $(this).attr("data-tab");
+    	$(".js-tab-cont").hide();
+    	$(".chat__icons a").removeClass("is-active");
+    	$(this).addClass("is-active");
+    	$(index).show();
+    	//alert(index);
+    });
+
+	
+
+
 });
